f6425964a02fbfa97a214d8b16f00f0c802a3e78
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2022-06-23 08:57:41 -0400

    SSR

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,45 @@
 import "./scss/app.scss";
 import {Route, Routes} from "react-router-dom";
+import Loadable from "react-loadable";
 import Home from "./pages/Home";
-// import Cart from "./pages/Cart";
-// import NotFound from "./pages/NotFound";
-// import FullPizza from "./pages/FullPizza";
+
 import MainLayout from "./layout/MainLayout";
 import {lazy, Suspense} from "react";
+import LoaderUI from "./components/LoaderUI";
 
-const Cart = lazy(() => import('./pages/Cart' /* webpackChunkName: "Cart" */))
-const FullPizza = lazy(() => import('./pages/FullPizza' /* webpackChunkName: "FullPizza" */))
-const NotFound = lazy(() => import('./pages/NotFound' /* webpackChunkName: "NotFound" */))
+const Cart = Loadable({
+    loader: () => import("./pages/Cart" /* webpackChunkName: "Cart" */),
+    loading: () => <LoaderUI/>,
+});
+const FullPizza = lazy(
+    () => import("./pages/FullPizza" /* webpackChunkName: "FullPizza" */)
+);
+const NotFound = lazy(
+    () => import("./pages/NotFound" /* webpackChunkName: "NotFound" */)
+);
 
 function App() {
-
     return (
-
-        <Routes>
-            <Route path="/" element={<MainLayout/>}>
-                <Route path="" element={<Home/>}/>
-                <Route path="cart" element={
-                    <Suspense fallback={<div><p>Loading... Cart</p></div>}>
-                        <Cart/>
-                    </Suspense>
-                }/>
-
-                <Route path="pizza/:id"
-                       element={
-                           <Suspense fallback={<div><p>Loading... Pizza</p></div>}>
-                               <FullPizza/>
-                           </Suspense>
-                       }/>
-
-                < Route path="*" element={
-                    <Suspense>
-                        <NotFound/>
-                    </Suspense>}
-                />
-            </Route>
-        </Routes>
-
-    )
-        ;
+        <Suspense fallback={<LoaderUI/>}>
+            <Routes>
+                <Route path="/" element={<MainLayout/>}>
+                    <Route path="" element={<Home/>}/>
+                    <Route
+                        path="cart"
+                        element={<Cart/>}
+                    />
+                    <Route
+                        path="pizza/:id"
+                        element={<FullPizza/>}
+                    />
+                    <Route
+                        path="*"
+                        element={<NotFound/>}
+                    />
+                </Route>
+            </Routes>
+        </Suspense>
+    );
 }
 
 export default App;
---
+++ src/components/LoaderUI.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+    Watch
+} from 'react-loader-spinner';
+
+const LoaderUI: React.FC = () => (
+    <Watch
+        color="#fe5f1e"
+        height="100"
+        width="110"
+        radius="40"
+    />
+);
+
+export default LoaderUI;
